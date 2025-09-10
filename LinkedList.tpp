119eff328070e182120ff2a5490ead8822212875
pavsidk <pav.sid.k.bay@gmail.com>
2025-09-09 22:44:34 -0700

    1

--- LinkedList.tpp
+++ LinkedList.tpp
@@ -10,16 +10,55 @@ LinkedList<T>::~LinkedList() {
 template <typename T>
 void LinkedList<T>::append(const T& elem) {
     // TODO
+    Node* t = new Node;
+    t->value = elem;
+    t->next = nullptr;
+    
+    if(head == nullptr) {
+        head = t;
+    }
+    else {
+        Node* curr = head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+
+        curr->next = t;
+    }
+    this->length+=1;
 }
 
 template <typename T>
 void LinkedList<T>::clear() {
     // TODO
+    Node* curr = head;
+    Node* t;
+    while(curr != nullptr) {
+        t = curr->next;
+        delete curr;
+        curr = t;
+    }
+    head = nullptr;
+    this->length=0;
 }
 
 template <typename T>
 T LinkedList<T>::getElement(int position) const {
     // TODO
+    Node* curr = head;
+    if (position < 0) {
+        throw string("getElement: error, position must be non-negative");
+    }
+    for (int i=0; i<=position; i++) {
+        if (curr == nullptr) {
+            throw string("getElement: error, position is out of bounds");
+        }
+        if (i == position) {
+            return curr->value;
+        }
+        curr = curr->next;
+    }
+    throw string("getElement: error, position is out of bounds");
 }
 
 template <typename T>
@@ -35,6 +74,20 @@ bool LinkedList<T>::isEmpty() const {
 template <typename T>
 void LinkedList<T>::replace(int position, const T& elem) {
     // TODO
+    Node* curr = head;
+    if (position < 0) {
+        throw string("replace: error, position must be non-negative");
+    }
+    for (int i=0; i<position; i++) {
+            if (curr == nullptr) {
+                throw string("replace: error, position is out of bounds");
+            }
+            curr = curr->next;
+    }
+    if (curr == nullptr) {
+        throw string("replace: error, position is out of bounds");
+    }
+    curr->value = elem;
 }
 
 template <typename T>
---
+++ driver.cpp
@@ -0,0 +1,75 @@
+#include "LinkedList.hpp"
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //creating the first linked list, for integers
+    LinkedList<int> myNumbers;
+    myNumbers.append(10);
+    myNumbers.append(43);
+    myNumbers.append(62);
+
+    //getting element and printing it
+    int number1 = myNumbers.getElement(1);
+    cout << "Element 1: " << number1 << endl;
+
+    //trying getElement as an invalid operation
+    try {
+        myNumbers.getElement(3);
+    }
+    catch (const string& e) {
+        cout << e << endl;
+    }
+
+    //printing an element before and after replacing it
+    cout << "Element 2, original: " << myNumbers.getElement(2) << endl;
+    myNumbers.replace(2, 100);
+    cout << "Element 2, replaced: " << myNumbers.getElement(2) << endl;
+
+    //trying replace as an invalid operation (negative position)
+    try {
+        myNumbers.replace(-3, 99);
+    }
+    catch (const string& e) {
+        cout << e << endl;
+    }
+
+    //trying replace as an invalid operation (position greater than length)
+    try {
+        myNumbers.replace(10, 99);
+    }
+    catch (const string& e) {
+        cout << e << endl;
+    }
+
+    //appending another number, and checking the length
+    cout << "Length before new append: " << myNumbers.getLength() << endl;
+    myNumbers.append(130);
+    cout << "Length after new append: " << myNumbers.getLength() << endl;
+
+    //clearing the linked list and checking
+    myNumbers.clear();
+    cout << "After clearing: ";
+    if (myNumbers.isEmpty()) {
+        cout << "The list is empty." << endl;
+    }
+    else {
+        cout << "The list is not empty." << endl;
+    }
+
+    //creating the second linked list, for 
+    LinkedList<string> myWords;
+    myWords.append("World");
+    myWords.append("Hello there!");
+    myWords.append("This is another string.");
+
+    //getting element 1
+    myWords.insert(1, "new word");
+    cout << "Element 1 from the 2nd linked list: " << myWords.getElement(1) << endl;
+
+    //printing the second linked list
+    cout << "2nd linked list, printed using overloaded operator: " << myWords << endl;
+}
+
